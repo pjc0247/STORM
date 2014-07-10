4c2e8f57060a10daef2b69340f704298463acc66
Park June Chul <pjc0247@naver.com>
2014-07-11 01:20:10 +0900

    Create Select.cpp

---
+++ example/Select.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "Sqb.h"
+
+
+int main(int argc,char **argv){
+	Sqb *query = Sqb::from("account");
+	
+	auto result = query
+		->where("id", "pjc0247")
+		->where_not_equal("nick", "pmnxis")
+		->select(3, string("level"), string("A"), string("B"))
+		->find_one();
+
+	printf("%s\n", result.c_str());
+	return 0;
+}
